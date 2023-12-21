837e8b042d45d58cdcd9bc09a1467c73cffb2b7c
saiful islam <gazifahad3@gmail.com>
2023-12-22 00:13:47 +0100

    home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./screens/Home";
+import { Footer } from "./components/Footer";
+import { Login } from "./screens/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/footer" element={<Footer />}></Route>
+      </Routes>
     </div>
   );
 }
